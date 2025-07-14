5d3279c103501073e260d80677b9f752e86e4460
Solomon Tolulope <kaysaki63@gmail.com>
2025-07-14 11:00:06 +0100

    first commit

---
+++ src/app/portfolio/Portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <div className="text-center portfolio text-white pt-8">
+        <p className='font-semibold'>Latest</p>
+        <h1 className='text-3xl font-extrabold'>Projects</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio;
